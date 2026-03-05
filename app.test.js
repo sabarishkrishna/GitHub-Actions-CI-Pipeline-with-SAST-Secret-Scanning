9d9ad185be29196a77cb438f4a591656254c7363
sabarishkrishna <77201176+sabarishkrishna@users.noreply.github.com>
2026-03-05 17:40:50 +0530

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../src/app');
+const app = require('./app');
 
 test('GET /health returns ok', async () => {
   const res = await request(app).get('/health');
